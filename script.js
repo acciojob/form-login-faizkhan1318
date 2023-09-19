b8d99a06cf5b18015f979562bb06205d9d01fed9
Faisel Khan <125124484+faizkhan1318@users.noreply.github.com>
2023-09-19 17:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function getFormvalue() {
     //Write your code here
-	alert("Rishabh Pant");
+	let element1 = document.getElementsByTagName("input")[0];
+	let element2 = document.getElementsByTagName("input")[1];
+
+	element1.value = "Rishabh";
+	element2.value = "Pant";
+	
+	alert("Rishabh Pant")
+	
 	
 
 }
